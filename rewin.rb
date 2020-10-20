6bc99ab1e2cd7a5c037e1ce8f79bf0b3daf67fe9
Moris Doratiotto <moris.doratiotto@gmail.com>
2020-10-20 14:27:24 +0200

    Update Brew formula

--- rewin.rb
+++ rewin.rb
@@ -1,7 +1,5 @@
 class Rewin < Formula
 
-  #include Language::Python::Virtualenv
-
   desc "A command line script for automatic reboot in BootCamp"
   homepage "https://github.com/Mortafix/homebrew-ReWin"
   url "https://github.com/Mortafix/homebrew-ReWin/archive/v1.0.0.tar.gz"
@@ -15,27 +13,14 @@ class Rewin < Formula
   require "fileutils"
 
   def install
-    #ENV.append "CPPFLAGS", "-I#{MacOS.sdk_path_if_needed}/usr/include/ffi"
 
-    # Installing python modules
-    #venv = virtualenv_create(libexec, Formula["python3"].opt_bin/"python3")
-    #venv.pip_install resources
-
-    # My python dependencies
-    #(libexec/"bin").install "config.json"
-    #(libexec/"bin"/"torrents").install "torrents/x1337.py"
-    #(libexec/"bin"/"torrents").install "torrents/limetorrents.py"
-    #(libexec/"bin"/"torrents").install "torrents/torlock.py"
-    #(libexec/"bin"/"torrents").install "torrents/torrentdownloads.py"
-    #(libexec/"bin"/"torrents").install "torrents/corsaronero.py"
-
-    # Final Script
+    # Script
     (libexec/"bin").install "RebootInWindows.py" => "rewin"
     FileUtils.chmod("+x", libexec/"bin/rewin")
     (bin/"rewin").write_env_script libexec/"bin/rewin", :PATH => "#{libexec}/bin:$PATH"
   end
 
   test do
-    system "gold-pirate -h"
+    system "rewin -h"
   end
 end
